5f9a4e82672b03c3603a9c1e0b955fd77a9e56ed
followtry <zhongzhijing@126.com>
2016-05-18 10:50:32 +0800

    新增笔记

---
+++ brief/brief-service/src/main/java/cn/jingzztech/prac/java/util/MapLearn.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package cn.jingzztech.prac.java.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author jingzz
+ * @time 2016年5月18日 上午10:32:47
+ * @func 
+ * @name Map
+ */
+public class MapLearn {
+	public static void main(String[] args) {
+		
+		Map<Object, Object> map = new HashMap<>();
+	}
+}
---
+++ brief/brief-service/src/main/java/cn/jingzztech/prac/java/util/test/MathTest.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package cn.jingzztech.prac.java.util.test;
+
+/**
+ * @author jingzz
+ * @time 2015年12月31日 上午11:07:23
+ * @func 
+ * @name MathTest
+ */
+public class MathTest {
+	public static void main(String[] args) {
+		
+	}
+}
