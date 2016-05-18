d10d6417f058ce2cf9a78dac236925da74875fd7
followtry <zhongzhijing@126.com>
2016-05-18 11:28:32 +0800

    重新配置项目

---
+++ brief/brief-comm/src/main/java/cn/jingzz/brief/comm/App.java
@@ -0,0 +1,13 @@
+package cn.jingzz.brief.comm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
