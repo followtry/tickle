930768561c94f47f54af2e4eedb4d7f015ec78b1
followtry <zhongzhijing@126.com>
2016-10-18 10:23:58 +0800

    添加对不存在的URL的处理； 添加根据不同参数访问不同请求；

--- brief/brief-springboot/src/main/java/cn/followtry/springboot/controller/HelloController.java
+++ brief/brief-springboot/src/main/java/cn/followtry/springboot/controller/HelloController.java
@@ -6,8 +6,14 @@ package cn.followtry.springboot.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Size;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,8 +32,16 @@ public class HelloController {
 	@Autowired
 	private Performance performance;
 	
+	//处理不存在的URL
+	@RequestMapping(value={"/**",})
+	public ResponseEntity<Object> root(){
+		Map<Object, Object> res = new HashMap<Object,Object>();
+		res.put("data", "404，未找到请求的资源");
+		return new ResponseEntity<Object>(res, HttpStatus.NOT_FOUND);
+	}
+	
 	@RequestMapping(value="/test")
-	public void test(@RequestParam("name")String name){
+	public void test(@RequestParam(value="name",required=false,defaultValue="荆中志")@Valid @Size(min=6) String name){
 		System.out.println("HelloController.test()");
 		performance.perform();
 		System.out.println();
@@ -36,10 +50,27 @@ public class HelloController {
 		performance.perform(name);
 		System.out.println("HelloController.test()3");
 	}
-	@RequestMapping(value={"/","/index"},produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	
+	//根据不同的参数进行不同的请求
+	@RequestMapping(value={"/","/index"},params={"name"},consumes=MediaType.APPLICATION_JSON_UTF8_VALUE,produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public Object rootHandler(){
 		Map<Object, Object> res = new HashMap<Object,Object>();
-		res.put("data", "this is my app end-point 哈哈");
+		res.put("data", "this is my app end-point 哈哈1");
+		return res;
+	}
+	
+	@RequestMapping(value={"/","/index"},produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public Object rootHandler2(){
+		Map<Object, Object> res = new HashMap<Object,Object>();
+		res.put("data", "this is my app end-point 哈哈2");
+		return res;
+	}
+	
+	@RequestMapping(value={"/model"},produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public Object modelHamder(Model model){
+		model.addAttribute("name", "jingzz");
+		Map<Object, Object> res = new HashMap<Object,Object>();
+		res.put("data", model);
 		return res;
 	}
 }
