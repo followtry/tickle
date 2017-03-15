8ad566e632486e74366ad89dbfac626ec356245f
followtry <zhongzhijing@126.com>
2017-03-15 15:05:43 +0800

    根据Spring规范自定义tag标签；

---
+++ brief/brief-spring/src/main/java/cn/followtry/custom/spring/tag/SimpleDateFormatBeanDefinitionParser.java
@@ -0,0 +1,34 @@
+package cn.followtry.custom.spring.tag;
+
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
+import org.springframework.util.StringUtils;
+import org.w3c.dom.Element;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * 当NameSpaceHander遇到xml元素类型映射到了指定的bean定义解析器时，对应的BeanDefinitionParser就会被使用。
+ * 在解析时，我们可以访问到XMl元素，因此我们可以解析到自定义的xml的内容
+ * Created by followtry on 2017/3/15.
+ */
+public class SimpleDateFormatBeanDefinitionParser extends AbstractSingleBeanDefinitionParser{
+
+	//指定需要实例化的bean的name
+	@Override
+	protected Class<?> getBeanClass(Element element) {
+		return SimpleDateFormat.class;
+	}
+
+	//解析命名空间下的自定义xml元素
+	@Override
+	protected void doParse(Element element, BeanDefinitionBuilder builder) {
+		String pattern=element.getAttribute("pattern");
+		builder.addConstructorArgValue(pattern);
+
+		String lenient=element.getAttribute("lenient");
+		if (StringUtils.hasText(lenient)){
+			builder.addPropertyValue("lenient",lenient);
+		}
+	}
+}
---
+++ brief/brief-spring/src/main/java/cn/followtry/custom/spring/tag/SimpleDateFormatNameSpaceHander.java
@@ -0,0 +1,19 @@
+package cn.followtry.custom.spring.tag;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+/**
+ * Created by followtry on 2017/3/15.
+ */
+public class SimpleDateFormatNameSpaceHander extends NamespaceHandlerSupport{
+	@Override
+	public void init() {
+		//此处可以注册多个Bean的解析器，在需要解析命名空间中的元素时将会代理的解析器
+		/*
+			这样清晰的关注点的分离可以使得一个Namespacehandler可以处理解析命名空间内所有自定义元素的编排。
+			它会将繁重的xml解析工作代理给指定的BeanDefinitionParsers，
+			这意味着每个BeanDefinitionParserker将会包含只解析单个自定义元素的逻辑。
+		 */
+		registerBeanDefinitionParser("dateformat",new SimpleDateFormatBeanDefinitionParser());
+	}
+}
