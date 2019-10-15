988f796510962019b39310b9a2510c4f9f74bbaa
jingzhongzhi <jingzhongzhi@meituan.com>
2019-10-15 23:32:47 +0800

    移除组件

--- brief-spring-all/src/main/java/cn/followtry/boot/BriefSpringbootApplication.java
+++ brief-spring-all/src/main/java/cn/followtry/boot/BriefSpringbootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@ComponentScan(basePackages = "cn.followtry.service")
+@ComponentScan(basePackages = "cn.followtry")
 public class BriefSpringbootApplication {
 
 	public static void main(String[] args) {
