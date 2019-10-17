06fdf6de7ec7e353fe649031ad2be0305b29e05b
jingzhongzhi <jingzhongzhi@meituan.com>
2019-10-18 00:00:05 +0800

    删除多余配置

--- brief-spring/brief-spring-all/src/main/java/cn/followtry/boot/BriefSpringbootApplication.java
+++ brief-spring/brief-spring-all/src/main/java/cn/followtry/boot/BriefSpringbootApplication.java
@@ -1,12 +1,10 @@
 package cn.followtry.boot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @ComponentScan(basePackages = "cn.followtry")
 public class BriefSpringbootApplication {
 
