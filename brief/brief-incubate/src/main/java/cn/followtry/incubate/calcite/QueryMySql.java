12197b09602ca358ea4432dd3936ede4a85eba78
jingzhongzhi <jingzhongzhi@meituan.com>
2020-03-30 11:15:03 +0800

    Mysql model 和 sql 读取文件

--- brief/brief-incubate/src/main/java/cn/followtry/incubate/calcite/QueryMySql.java
+++ brief/brief-incubate/src/main/java/cn/followtry/incubate/calcite/QueryMySql.java
@@ -1,7 +1,12 @@
 package cn.followtry.incubate.calcite;
 
 import org.apache.calcite.jdbc.CalciteConnection;
+import org.apache.commons.io.FileUtils;
+import org.springframework.core.io.FileSystemResource;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -12,39 +17,27 @@ import java.util.Properties;
 /**
  * calcite 连接 mysql <a>https://blog.csdn.net/hjw199089/article/details/82717377</a>
  * 1. calcite 的连接属性设置。 来源于{@link org.apache.calcite.config.CalciteConnectionProperty}，如 info.setProperty("lex", "JAVA");即将词法设置为 java
+ *
  * @author jingzhongzhi
  * @since 2020/3/29
  */
 public class QueryMySql {
+    
+    public static final String basePath = "/Users/jingzhongzhi/git-project/tickle/brief/brief-incubate/src/main/resources";
+    
     /**
-     *
      * 1. calcite 的连接属性设置。 来源于{@link org.apache.calcite.config.CalciteConnectionProperty}，如 info.setProperty("lex", "JAVA");即将词法设置为 java
      * main.
      */
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
         Class.forName("org.apache.calcite.jdbc.Driver");
         Properties info = new Properties();
         //连接词法为Mysql
         info.setProperty("lex", "MYSQL");
+        File modelFile = new FileSystemResource(basePath + "/calcite/model.json").getFile();;
+        String modelJson = FileUtils.readFileToString(modelFile, StandardCharsets.UTF_8);
         //连接 Mysql 的模型
-        info.setProperty("model","inline:" +
-                "{\n" +
-                "  version: '1.0',\n" +
-                "  defaultSchema: 'brief',\n" +
-                "  schemas: [\n" +
-                "    {\n" +
-                "      name: 'brief',\n" +
-                "      type: 'custom',\n" +
-                "      factory: 'org.apache.calcite.adapter.jdbc.JdbcSchema$Factory',\n" +
-                "      operand: {\n" +
-                "        jdbcDriver: 'com.mysql.jdbc.Driver',\n" +
-                "        jdbcUrl: 'jdbc:mysql://localhost:3306/brief',\n" +
-                "        jdbcUser: 'brief',\n" +
-                "        jdbcPassword: 'brief'\n" +
-                "      }\n" +
-                "    }\n" +
-                "  ]\n" +
-                "}");
+        info.setProperty("model", "inline:" + modelJson);
         //连接的 url，通过判断前缀是否为jdbc:calcite: 或  jdbc:avatica:remote:来判断是否使用 calcite
         String connUrl = "jdbc:calcite:";
         Connection connection = DriverManager.getConnection(connUrl, info);
@@ -53,15 +46,16 @@ public class QueryMySql {
         ResultSet tables = calciteConnection.getMetaData().getTables(null, null, null, null);
         printResult(tables);
         Statement statement = calciteConnection.createStatement();
-        String sql = "select u.id,u.name,u.age " +
-                "from user_info as u where u.id < 100";
+    
+        File sqlFile = new FileSystemResource(basePath + "/calcite/mysql.sql").getFile();;
+        String sql = FileUtils.readFileToString(sqlFile, StandardCharsets.UTF_8);
         ResultSet resultSet = statement.executeQuery(sql);
         printResult(resultSet);
-    
+        
         resultSet.close();
         statement.close();
         connection.close();
-    
+        
     }
     
     private static void printResult(ResultSet resultSet) throws SQLException {
---
+++ brief/brief-incubate/src/main/resources/calcite/mysql.sql
@@ -0,0 +1,3 @@
+select u.id,u.name,u.age
+from user_info as u
+where u.id < 100
