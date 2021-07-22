2b4530fca4b5962a8cad0aa2aa71f445a97f328f
qiuluo <zhongzhi.jzz@alibaba-inc.com>
2021-07-22 11:43:07 +0800

    kotlin service

---
+++ brief-springboot-kotlin/src/main/kotlin/cn/followtry/component/KHelloService.kt
@@ -0,0 +1,35 @@
+package cn.followtry.component
+
+import cn.followtry.HelloService
+import org.springframework.beans.factory.BeanFactory
+import org.springframework.beans.factory.BeanFactoryAware
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.context.ApplicationContext
+import org.springframework.context.ApplicationContextAware
+import org.springframework.stereotype.Service
+
+/**
+ *
+ * @author followtry
+ * @since  2021/7/22 11:37 上午
+ */
+@Service
+@ConditionalOnBean(name = ["helloServiceImpl"])
+open class KHelloService : HelloService,BeanFactoryAware,ApplicationContextAware{
+
+    private lateinit var beanFactory: BeanFactory
+    private lateinit var ac: ApplicationContext
+
+    override fun say(content: String): String {
+        return content
+    }
+
+    override fun setBeanFactory(bf: BeanFactory) {
+        this.beanFactory = bf
+    }
+
+    override fun setApplicationContext(applicationContext: ApplicationContext) {
+        this.ac = applicationContext
+        println(ac.getBean("helloServiceImpl",HelloService::class.java).say("我是java的hello实现，被kotlin调用了"))
+    }
+}
--- brief-springboot-kotlin/src/main/kotlin/cn/followtry/web/TestController.kt
+++ brief-springboot-kotlin/src/main/kotlin/cn/followtry/web/TestController.kt
@@ -2,6 +2,7 @@ package cn.followtry.web
 
 import cn.followtry.HelloService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController
 open class TestController {
 
     @Autowired
+    @Qualifier(value = "KHelloService")
     private lateinit var helloService : HelloService
 
     @GetMapping(value = ["hello"])
