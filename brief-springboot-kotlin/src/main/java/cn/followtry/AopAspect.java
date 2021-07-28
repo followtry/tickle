02256e70b0c7f11d8aec114a3db2f6ad8f254516
qiuluo <zhongzhi.jzz@alibaba-inc.com>
2021-07-28 10:18:16 +0800

    feat（aop）：切面原理

--- brief-springboot-kotlin/src/main/java/cn/followtry/AopAspect.java
+++ brief-springboot-kotlin/src/main/java/cn/followtry/AopAspect.java
@@ -1,12 +1,11 @@
 package cn.followtry;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 /**
+ * 切面实现原理参考： <https://blog.csdn.net/supzhili/article/details/98401855/>
  * @author followtry
  * @since 2021/7/27 4:47 下午
  */
@@ -20,9 +19,26 @@ public class AopAspect {
 
     @Around(value = "pointCut()")
     public Object aroundProcess(ProceedingJoinPoint joinPoint) throws Throwable {
-        System.out.println("代码前执行");
+        System.out.println("aroundProcess before");
         Object proceed = joinPoint.proceed();
-        System.out.println("代码后执行");
+        System.out.println("aroundProcess after");
         return proceed;
     }
+
+    @Before(value = "pointCut()")
+    public void beforeProcess() {
+        System.out.println("beforeProcess");
+    }
+
+    @After(value = "pointCut()")
+    public void afterProcess() {
+        System.out.println("afterProcess");
+    }
+
+    @AfterReturning(value = "pointCut()")
+    public void afterReturnProcess() {
+        System.out.println("afterReturnProcess");
+    }
+
+
 }
